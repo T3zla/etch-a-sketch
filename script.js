613c51dc4c06d3e8694d1a0582d23bb4f39138e3
T3zla <alzettes@yahoo.com>
2025-06-29 17:28:48 +0800

    Create basic grid

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++){
+    const grid = document.createElement("div");
+    grid.setAttribute("class", "square");
+    container.appendChild(grid);
+    console.log("div made")
+}
+
